2cc2ca7f55ef68bd050cde660a7d138000a7079f
Steve Kimoi <moenisteve@gmail.com>
2022-12-01 16:31:07 +0300

    testing the mint function

--- test/bored-ape.test.ts
+++ test/bored-ape.test.ts
@@ -27,4 +27,15 @@ describe("Bored Ape", () => {
    it("Owner of the contract must be the one who deployed the contract", async () => {
        expect(await boredApeContract.owner()).to.equal(await owner.address); 
    }); 
+
+//    it("Should ensure Ape price is equal to 0.08 eth", async () => {
+//        expect(await boredApeContract.apePrice()).to.equal("80000000000000000"); 
+//    }); 
+
+   it("Should mint an ape", async () => {
+       await boredApeContract.flipSaleState(); 
+       const apePrice = await boredApeContract.apePrice(); 
+       expect(await boredApeContract.mintApe(1, {value: apePrice})).to.emit(boredApeContract, "Transfer")
+       .withArgs(ethers.constants.AddressZero, owner.address, 1); 
+   }); 
 })
