a2e8d2598cfbdc1b7ba8dfcbbd692914f4d4c26d
Steve Kimoi <moenisteve@gmail.com>
2022-12-01 16:03:21 +0300

    using beforeEach from mocha

--- test/bored-ape.test.ts
+++ test/bored-ape.test.ts
@@ -1,15 +1,30 @@
 import { ethers } from "hardhat"; 
 import { expect } from "chai"; 
+import { Contract } from "ethers"; 
+import { beforeEach } from "mocha"; 
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers"
 
 describe("Bored Ape", () => {
+    let boredApeContract: Contract;  
+    let owner: SignerWithAddress; 
+    let address1: SignerWithAddress; 
+
+    beforeEach( async () => {
+        const boredApeFactory = await ethers.getContractFactory("BoredApeYachtClub"); 
+        [owner, address1] = await ethers.getSigners(); 
+        boredApeContract = await boredApeFactory.deploy(
+            "Bored Ape Yacht Club", 
+            "BAYC", 
+            10000, 
+            1
+        ); 
+    })
+
    it("Should initialize Bored Ape Contract", async () => {
-       const boredApeFactory = await ethers.getContractFactory("BoredApeYachtClub"); 
-       const boredApeContract = await boredApeFactory.deploy(
-           "Bored Ape Yacht Club", 
-           "BAYC", 
-           10000, 
-           1
-       ); 
        expect(await boredApeContract.MAX_APES()).to.equal(10000); 
-   })
+   }); 
+
+   it("Owner of the contract must be the one who deployed the contract", async () => {
+       expect(await boredApeContract.owner()).to.equal(await owner.address); 
+   }); 
 })
